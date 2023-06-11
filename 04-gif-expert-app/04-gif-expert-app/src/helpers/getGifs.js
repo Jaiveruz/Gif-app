0e876a35ff579d88b54a773e9e0b6fa5b5cbace8
Jaiver <Jaiver.uz2@gmail.com>
2023-06-10 21:51:51 -0400

    agregando pruebas desde la casa

--- 04-gif-expert-app/04-gif-expert-app/src/helpers/getGifs.js
+++ 04-gif-expert-app/04-gif-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=MlDsfibijTE35lwfN2I4T4RWvtipksm5&q=${category}&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=MlDsfibijTE35lwfN2I4T4RWvtipksm5&q=${category}&limit=5`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     
